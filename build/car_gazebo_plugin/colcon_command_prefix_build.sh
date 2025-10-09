02f4e5fd9eb56bfa4fae4533f09b875a415d2b6d
Joran <joranfloor@gmail.com>
2025-10-09 17:10:40 +0200

    commit

---
+++ build/car_gazebo_plugin/colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/yappel/ros2_ws/install/ackermann_msgs/share/ackermann_msgs/package.sh"
---
+++ build/smartcar_msgs/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/yappel/ros2_ws/src/smartcar_msgs
+# Build directory: /home/yappel/ros2_ws/build/smartcar_msgs
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
